5f4b48704624692d0f02206187928ef334f8c3ca
emoose0 <139067813+emoose0@users.noreply.github.com>
2023-07-12 22:34:52 -0400

    Update main.py
    - Added support for NativeTrainer.asi
    - Added support for ScriptHookRDR2.dll
    - Turned file renaming process into a function

--- main.py
+++ main.py
@@ -1,39 +1,25 @@
 import os
 
-path = "dinput8.dll"
-path2 = "dinput8.dll.disabled"
-fileChecker = os.path.isfile(path)
+def enden(fileName):
+    fileName2 = fileName + ".disabled"
+    fileChecker = os.path.isfile(fileName)
 
-if fileChecker:
-    print("dinput8.dll found!\ndisabling file...")
-    os.rename(path, path2)
-    print("\ndinput8.dll successfully disabled!\n")
-elif not fileChecker:
-    filechecker = os.path.isfile(path2)
-    if filechecker:
-        print("dinput8.dll.disabled found!\nenabling file...")
-        os.rename(path2, path)
-        print("\ndinput8.dll successfully enabled!\n")
-    else:
-        print("DINPUT8.DLL NOT FOUND\ntrying to find version.dll...\n")
+    print("trying to find " + fileName + "...")
 
-path = "version.dll"
-path2 = "version.dll.disabled"
-fileChecker = os.path.isfile(path)
+    if fileChecker:
+        print(fileName + " found!\ndisabling file...")
+        os.rename(fileName, fileName2)
+        print("\n" + fileName + " successfully disabled!\n")
+    elif not fileChecker:
+        filechecker = os.path.isfile(fileName2)
+        if filechecker:
+            print(fileName2 + " found!\nenabling file...")
+            os.rename(fileName2, fileName)
+            print("\n" + fileName + "successfully enabled!\n")
+        else:
+            print(fileName.upper() + " NOT FOUND\n")
 
-if fileChecker:
-    print("version.dll found!\ndisabling file...")
-    os.rename(path, path2)
-    print("\nversion.dll successfully disabled!")
-    input("press enter to terminate program ")
-elif not fileChecker:
-    filechecker = os.path.isfile(path2)
-    if filechecker:
-        print("version.dll.disabled found!\nenabling file...")
-        os.rename(path2, path)
-        print("\nversion.dll successfully enabled!")
-        input("\npress enter to terminate program")
-    else:
-        print("VERSION.DLL NOT FOUND")
-        input("\npress enter to terminate program")
-
+enden("dinput8.dll")
+enden("version.dll")
+enden("NativeTrainer.asi")
+enden("ScriptHookRDR2.dll")
